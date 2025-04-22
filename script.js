d91d88c2f471a064a8f947e0f94b0811a3fd950f
RayqRohit <rrajput1172@gmail.com>
2025-04-22 11:07:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    
+    mobileMenuBtn.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+    });
+});
